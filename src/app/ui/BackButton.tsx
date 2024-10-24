89bdd01d761a3b931f41f6c35057e8a5deb2fbde
ThibSara <sara.thibierge@gmail.com>
2024-10-24 16:47:53 +0200

    Refactor BackButton component to use relative path for home link

--- src/app/ui/BackButton.tsx
+++ src/app/ui/BackButton.tsx
@@ -1,13 +1,12 @@
 "use client";
 import { ArrowLeft } from "lucide-react";
-import PAGE_NAMES from "../utils/PAGE_NAMES";
 import Link from "next/link";
 import { useEffect } from "react";
 
 export const BackButton = () => {
   const handleKeyDown = (event: KeyboardEvent) => {
     if (event.key === "Escape") {
-      window.location.href = PAGE_NAMES.HOME;
+      window.location.href = "/";
     }
   };
 
@@ -21,7 +20,7 @@ export const BackButton = () => {
 
   return (
     <Link
-      href={PAGE_NAMES.HOME}
+      href={"/"}
       className="flex flex-row items-center gap-x-2 group"
       aria-label="Go back to home page"
     >
