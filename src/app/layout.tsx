5dd60b2e257a25b3d5a8d9b8dfd117394fb90109
ThibSara <sara.thibierge@gmail.com>
2024-10-24 16:35:25 +0200

    Update layout and page structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./index.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-h-screen ">
+          <Navbar />
+          <main className="px-20 sm:px-60 xl:px-120">{children}</main>
+        </div>
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+import data from "./../data/index.json";
+import HeroSection from "./components/HeroSection";
+import GridSection from "./components/GridSection";
+
 export default function Home() {
-  return <main>Valtech Front Challenge</main>;
+  return (
+    <div className="py-60 sm:py-120">
+      {data.content.map((section, index) => {
+        if (section.type === "HERO") {
+          return (
+            <HeroSection
+              key={index}
+              title={section.title}
+              subtitle={section.subtitle}
+              description={section.description}
+              backgroundAsset={section.backgroundAsset}
+              foregroundAsset={section.foregroundAsset}
+            />
+          );
+        }
+        if (section.type === "CARD_GRID") {
+          return (
+            <div className="pt-80">
+              <GridSection title={section.title} cards={section.cards} />
+            </div>
+          );
+        }
+        return null;
+      })}
+    </div>
+  );
 }
